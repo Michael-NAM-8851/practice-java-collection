c44062dd7ed1fe12b784439f73af99fbfb89f5dd
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-11-30 14:27:32 +0800

    feat: finish filter, map, reduce and supplement

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -5,14 +5,22 @@ import java.util.List;
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return 0;
+        return numbers
+                .stream()
+                .reduce((lastOddNum, currNum) -> currNum % 2 == 1 ? currNum : lastOddNum)
+                .get();
     }
 
     public String getLongest(List<String> words) {
-        return null;
+        return words
+                .stream()
+                .reduce((longestWord, word) -> word.length() > longestWord.length() ? word : longestWord)
+                .get();
     }
 
     public int getTotalLength(List<String> words) {
-        return 0;
+        return words
+                .stream()
+                .reduce(0, (accumulator, word) -> accumulator + word.length(), Integer::sum);
     }
 }
